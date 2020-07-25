d4b5fff2efa0854f86f191583bcee2ee823b8ac6
AuDietrich <33529775+ASP-React-Database@users.noreply.github.com>
2020-07-24 20:21:32 -0700

    base version working

---
+++ src/main/java/personal/portfolio/api/PortfolioController.java
@@ -0,0 +1,14 @@
+package personal.portfolio.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class PortfolioController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/")
+	public String index() {
+		return "homePage";
+	}
+}
