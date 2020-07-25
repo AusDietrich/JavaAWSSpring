264a0e5d0e10800d94c4d2da4c3462c4f1321f6a
AuDietrich <33529775+ASP-React-Database@users.noreply.github.com>
2020-07-24 20:04:08 -0700

    base push

---
+++ src/main/java/personal/portfolio/PortfolioApplication.java
@@ -0,0 +1,13 @@
+package personal.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApplication.class, args);
+	}
+
+}
---
+++ src/main/java/personal/portfolio/api/Controller.java
@@ -0,0 +1,13 @@
+package personal.portfolio.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/")
+	public String index() {
+		return "homePage";
+	}
+}
